4baa4e5c95e21ea389b71e931750c512e3f7ca11
Shyzuna <villain.benoit.dev@gmail.com>
2019-12-09 14:10:49 +0100

    Removing old zmq

--- AlternateControler/BlockCommunicator.py
+++ AlternateControler/BlockCommunicator.py
@@ -1,4 +1,3 @@
-import zmq
 import params.Params as Params
 import multiprocessing as mp
 import os
@@ -6,30 +5,6 @@ import socket
 import re
 from AlternateControler.NetworkMessageType import NetworkMessageType
 import time
-
-def ZmqCom(comPipe):
-    print("Com Process started : {}".format(os.getpid()))
-    context = zmq.Context()
-    socket = context.socket(zmq.REP)
-    socket.bind("tcp://{}:{}".format(Params.HOST, Params.PORT))
-    print("Binded to tcp://{}:{}".format(Params.HOST, Params.PORT))
-    while True:
-        message = socket.recv()
-        print("Received from client {}:{}".format(os.getpid(), message))
-
-        '''try:
-            pipeVal = comPipe.get_nowait()
-            print("{}:{}".format(os.getpid(), pipeVal))
-        except Exception as e:
-            pass
-
-        comPipe.put(10, False)'''
-
-        if comPipe.poll():
-            pipeVal = comPipe.recv()
-            print("Received {}:{}".format(os.getpid(), pipeVal))
-
-        socket.send(b"Tutu")
 
 def TcpSocketCom(comPipe):
     inOut = "0,0"
